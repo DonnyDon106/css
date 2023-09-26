d7766b5c65f8df5d6cdb5deb80d1a984efe8d81a
DonnyDon106 <satpalgahlo0@gmail.com>
2023-09-26 16:19:47 +0530

    first commit

---
+++ script.js
@@ -0,0 +1,39 @@
+function ris() {
+    let r = 0
+    if (document.getElementById("a1").checked) {
+        r++
+    }
+    if (document.getElementById("b3").checked) {
+        r++
+    }
+    if (document.getElementById("c2").checked) {
+        r++
+    }
+    if (document.getElementById("d1").checked) {
+        r++
+    }
+    if (document.getElementById("e3").checked) {
+        r++
+    }
+
+    if (document.getElementById("f2").checked) {
+        r++
+    }
+    if (document.getElementById("g2").checked) {
+        r++
+    }
+    if (document.getElementById("h1").checked) {
+        r++
+    }
+    if (document.getElementById("i2").checked) {
+        r++
+    }
+    if (document.getElementById("j3").checked) {
+        r++
+    }
+    alert("Result=" + r + "/10");
+}
+
+function ch() {
+    document.getElementById("f1") = checked;
+}
